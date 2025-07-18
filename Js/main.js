9afb75c46c1ed927d6db3ba5f4afdb3f57a38d45
Aya Hamed <76251786+20180152aya@users.noreply.github.com>
2025-07-19 01:38:02 +0300

    Add files via upload

---
+++ Js/main.js
@@ -0,0 +1,10 @@
+function showmenu() {
+    const sidebar =document.querySelector('.sidebar');
+    if(sidebar.style.display == 'none'){
+        sidebar.style.display = 'flex';
+    }else{
+        sidebar.style.display = 'none';
+    }
+    
+    
+}  
